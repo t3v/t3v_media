7ee3586f0d4f1284fef1b7438496be98e2c8053d
Maik Kempe <mkempe@bitaculous.com>
2018-06-14 22:35:23 +0200

    [✚] Added common files. [❶] First commit.

---
+++ ext_emconf.php
@@ -0,0 +1,34 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+  'title' => 'T3v Media',
+  'description' => 'The media extension of TYPO3Voila.',
+  'author' => 'Maik Kempe',
+  'author_email' => 'mkempe@bitaculous.com',
+  'author_company' => 'Bitaculous - It\'s all about the bits, baby!',
+  'category' => 'fe',
+  'state' => 'stable',
+  'version' => '0.0.1',
+  'createDirs' => '',
+  'uploadfolder' => false,
+  'clearCacheOnLoad' => false,
+  'constraints' => [
+    'depends' => [
+      'typo3' => '8.7.0-9.5.99',
+      't3v_core' => '',
+      't3v_content' => ''
+    ],
+    'conflicts' => [
+    ],
+    'suggests' => []
+  ],
+  'autoload' => [
+    'psr-4' => [
+      'T3v\\T3vMedia\\' => 'Classes'
+    ]
+  ],
+  'autoload-dev' => [
+    'psr-4' => [
+      'T3v\\T3vMedia\\Tests\\' => 'Tests'
+    ]
+  ]
+];
