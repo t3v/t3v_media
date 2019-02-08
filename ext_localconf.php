fdf8c672927c7f755235a7dd310a8cbc8464f08e
Maik Kempe <mkempe@bitaculous.com>
2019-02-08 21:34:33 +0100

    [➦] Merged changes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,16 +8,16 @@ $iconsFolder  = \T3v\T3vCore\Utility\ExtensionUtility::iconsFolder($extensionKey
 
 // === Youku Integration ===
 
-// Add Youku as new mime type
+// Add Youku as new mime type.
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType']['youku'] = 'video/youku';
 
-// Add Youku to allowed media file extensions
+// Add Youku to allowed media file extensions.
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'] .= ',youku';
 
-// Register the Youku helper
+// Register the Youku helper.
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['youku'] = \T3v\T3vMedia\Resource\OnlineMedia\Helpers\YoukuHelper::class;
 
-// Register the Youku renderer
+// Register the Youku renderer.
 $rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
 $rendererRegistry->registerRendererClass(\T3v\T3vMedia\Resource\Rendering\YoukuRenderer::class);
 
