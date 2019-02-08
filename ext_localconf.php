e0bc1aa57dfe3b77c049b5cd39fb9979fec29922
Maik Kempe <mkempe@bitaculous.com>
2019-02-08 21:42:15 +0100

    [♻] Cleaned up.

--- ext_localconf.php
+++ ext_localconf.php
@@ -24,15 +24,15 @@ $rendererRegistry->registerRendererClass(\T3v\T3vMedia\Resource\Rendering\YoukuR
 // === Backend ===
 
 if (TYPO3_MODE === 'BE') {
+  // --- Icons ---
+
   $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
 
-  // Register the icon for the Youku mime type
   $iconRegistry->registerIcon(
     'mimetypes-media-video-youku',
     \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
     ['source' => "{$iconsFolder}/MimeTypes/Youku.svg"]
   );
 
-  // Register the file extension icon
   $iconRegistry->registerFileExtension('youku', 'mimetypes-media-video-youku');
 }
